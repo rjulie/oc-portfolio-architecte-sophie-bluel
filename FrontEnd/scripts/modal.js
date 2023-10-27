391f634e31178c86804a4f825a1696850fcf7fc2
Julie Reynal <rjulie.pro@gmail.com>
2023-10-27 11:11:49 +0200

    3.3 step: fetch post and delete working

--- FrontEnd/scripts/modal.js
+++ FrontEnd/scripts/modal.js
@@ -24,6 +24,10 @@ function generateIndex(works) {
 
       trashElement.dataset.id = works[i].id;
       trashElement.classList.add("js-delete-btn");
+
+      // fonction callback nécessaire (arrow function)
+      trashElement.addEventListener("click", (event) => deleteWork(event, workElement));
+
       trashElement.classList.add("delete-btn");
       trashElement.innerHTML=`<i class="fas fa-trash-alt"></i>`;
 
@@ -37,31 +41,40 @@ function generateIndex(works) {
 
 /////////// DELETE ////////////////
 
-async function fetchDelete(workId) {
-  await fetch(`http://localhost:5678/api/works/${workId}`, { method: 'DELETE' });
+async function fetchDelete(userToken, workId) {
+  // passer le bearer
+  return await fetch(`http://localhost:5678/api/works/${workId}`, {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${userToken}`,
+    },
+  });
 }
 
-function deleteWork() {
-  const buttonsDelete = document.querySelectorAll(".js-delete-btn");
+async function deleteWork(event, workElement) {
 
-  buttonsDelete.forEach((button) =>
-    button.addEventListener("click",() => {
-      try {
-        const workId = button.dataset.id;
-        const work = document.getElementById(`work-project-${workId}`);
-        const response = fetchDelete(workId);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        work.remove();
+  console.log(workElement);
 
-      } catch(error) {
-        // message erreur projet non supprimé
-        window.alert("Le projet n'a pas pu être supprimé.");
-      }
-    })
-  );
+  try {
+    const workId = workElement.dataset.id;
+    const work = document.getElementById(`work-project-${workId}`);
+    const userToken = window.localStorage.getItem("token").replace(/['"]+/g, '');
+    const response = await fetchDelete(userToken, workId);
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    work.remove();
+
+  } catch(error) {
+    console.log(error);
+    // message erreur projet non supprimé
+    window.alert("Le projet n'a pas pu être supprimé.");
+  }
+
 }
+
+
 
 
 ///////////// NEW ////////////////
@@ -96,16 +109,34 @@ function setPreviousImage() {
   }
 }
 
-async function fetchPost(userToken, newWorkJson) {
-  await fetch("http://localhost:5678/api/works", {
+async function fetchPost(userToken, formData) {
+  // return important pour recevoir la réponse du fetch
+  return await fetch("http://localhost:5678/api/works", {
     method: "POST",
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json',
       'Authorization': `Bearer ${userToken}`,
     },
-    body: newWorkJson,
+    body: formData,
   });
+}
+
+function addElementOnGallery(response) {
+  // Création et ajout du work
+  const sectionGallery = document.querySelector('.gallery');
+  const workElement = document.createElement("figure");
+  workElement.dataset.id = response.id;
+  const imageUrl = response.imageUrl;
+  const title = response.title ?? "(aucun titre)";
+  workElement.innerHTML=
+    `<img src="${imageUrl}" alt="${title}">
+    <figcaption> ${title}</figcaption>`
+  ;
+  sectionGallery.appendChild(workElement);
+
+  // Fermeture de la modal
+  const modal = document.querySelector('#modal');
+  modal.close();
 }
 
 function createProject() {
@@ -119,54 +150,45 @@ function createProject() {
   buttonValidate.classList.add("js-validate-btn");
   footer.insertAdjacentHTML("afterbegin", buttonValidate.outerHTML);
 
-  const buttonPost = document.querySelector(".js-validate-btn");
+  // const buttonPost = document.querySelector(".js-validate-btn");
+  const newForm = document.getElementById("form-new");
 
-  buttonPost.addEventListener("click", () => {
+  newForm.addEventListener("submit", async (event) => {
     try {
+      event.preventDefault();
       console.log("fetch");
-
-      const lastId = works.slice(-1)[0].id;
-      const titleValue = document.getElementById("title").value;
-      // const imageValue = document.getElementById("preview-image").src;
-      const categoryValue = document.getElementById("category").value;
-      const categoryId = (categories.find((category) => category.name === categoryValue)).id;
-      const userId = window.localStorage.getItem("id");
       const userToken = window.localStorage.getItem("token").replace(/['"]+/g, '');
 
-      // enregister dans une constante l'url de l'image
-      // la console logger
-      // tester de passer le fetch
-      // const fileInput = document.getElementById("photo");
-      // const formData = new FormData();
-      // formData.append('id', lastId + 1);
-      // formData.append('title', titleValue);
-      // formData.append('imageUrl', fileInput.files[0]);
-      // formData.append('categoryId', parseInt(categoryId));
-      // formData.append('userId', parseInt(userId));
+      const formData = new FormData(newForm);
 
-      const newWork = {
-        id: lastId + 1,
-        title: `${titleValue}`,
-        imageUrl: `http://localhost:5678/images/${imageUrl}`,
-        categoryId: parseInt(categoryId),
-        userId: parseInt(userId),
-      };
-
-      // Création du newWork au format JSON
-      const newWorkJson = JSON.stringify(newWork);
-      console.log(newWorkJson);
+      // On peut cacher l'élément avec propriété > visible
+      // ou variable
+      // const photo = inputPhoto.files[0]
 
       // Appel de la fonction fetch avec toutes les informations nécessaires
-      const response = fetchPost(userToken, newWorkJson);
-      console.log(response);
+      const response = await fetchPost(userToken, formData);
+
+      // il faut le json pour un fetch et refabriquer l'objet en json et l'utiliser si besoin
+      const parsedResponse = await response.json();
+      console.log(parsedResponse);
+
+      // j'ajoute à partir de ma response
+      // j'ajoute l'élément dans ma section icons-gallery content
+      // et je ferme ma modal
+      addElementOnGallery(parsedResponse);
+
+      // corrige le preview de la photo
+
+      // et le bouton valider
+
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
     } catch(error) {
       // message erreur projet non ajouté
+      console.log(error);
       window.alert("Le projet n'a pas pu être ajouté.");
     }
-
   })
 }
 
@@ -195,7 +217,7 @@ function addProject() {
         <form action="#" method="post" class="add-form form-modal" id="form-new">
           <div class="input-image">
             <i class="fa-regular fa-image"></i>
-            <input type="file" id="photo" name="photo" accept="image/png, image/jpeg"/>
+            <input type="file" id="photo" name="image" accept="image/png, image/jpeg"/>
             <label id="photo-label" for="photo">
               + Ajouter photo
             </label>
@@ -211,6 +233,7 @@ function addProject() {
               <option value=""></option>
             </select>
           </div>
+          <input type="submit" value="Ajout">
         </form>
       `;
 
@@ -219,7 +242,7 @@ function addProject() {
         const selectElement = document.getElementById("category");
 
         selectValue.innerHTML =`${category.name}`;
-        selectValue.setAttribute("value", `${category.name}`);
+        selectValue.setAttribute("value", `${category.id}`);
         selectElement.insertAdjacentHTML("beforeend", selectValue.outerHTML);
       });
 
